659945efc26b15d89c1a0c0b3d727757617eb204
oleg-kaluga2001@mail.ru <44f91c29ec4567654aA>
2020-11-15 04:16:40 +0300

    Updates

--- precache-manifest.c9ac1cbbc4d0c94c9e48370392d0b722.js
+++ precache-manifest.c9ac1cbbc4d0c94c9e48370392d0b722.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9a020d77d0b1391725e2caaab52b29ba",
+    "revision": "eb6166f8ff2261fb14ae5f7b0a288cfb",
     "url": "/index.html"
   },
   {
-    "revision": "5ff5cfb687fdce081bce",
+    "revision": "e1085f017ad8bebfa424",
     "url": "/static/css/2.86df01db.chunk.css"
   },
   {
-    "revision": "5ff5cfb687fdce081bce",
-    "url": "/static/js/2.97221985.chunk.js"
+    "revision": "e1085f017ad8bebfa424",
+    "url": "/static/js/2.8e9a9ba3.chunk.js"
   },
   {
     "revision": "1c9a5c14c094f70c35a5a57a5770d494",
-    "url": "/static/js/2.97221985.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.8e9a9ba3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "09bc2f5028b354655a52",
-    "url": "/static/js/main.91899d11.chunk.js"
+    "revision": "88a09ca002277d2ff016",
+    "url": "/static/js/main.0da2e4e0.chunk.js"
   },
   {
     "revision": "d5d50483e52d53a4b219",
